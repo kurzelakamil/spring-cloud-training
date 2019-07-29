0c63a55f15278b91fa8e0e38e2eeb050432328ad
landrzejewski <landrzejewski.poczta@gmail.com>
2019-07-29 06:44:15 +0200

    Base project

--- departments/src/main/java/pl/training/cloud/departments/service/DepartmentNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package pl.training.cloud.departments.service;
-
-public class DepartmentNotFoundException extends RuntimeException {
-}
--- users/src/main/java/pl/training/cloud/users/service/DepartmentsService.java
+++
@@ -1,9 +0,0 @@
-package pl.training.cloud.users.service;
-
-import java.util.Optional;
-
-public interface DepartmentsService {
-
-    Optional<String> getDepartmentName(Long departmentId);
-
-}
