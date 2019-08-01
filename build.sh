b36695053d38f9ca4e639a2337eb84102344c1eb
landrzejewski <landrzejewski.poczta@gmail.com>
2019-08-01 08:27:46 +0200

    aa

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-docker rmi $(dokcer images -q | grep "^training")
-docker rmi $(dokcer images -q | grep "<none>")
+docker rmi $(docker images | grep "^training")
+docker rmi $(docker images | grep "<none>")
 
 ./mvnw clean package
 
 docker build -t "training/configuration-server" configuration
 docker build -t "training/discovery-server" discovery
-docker build -t "training/gateway-server" discovery
-docker build -t "training/zipkin-server" discovery
+docker build -t "training/gateway-server" gateway
+docker build -t "training/zipkin-server" zipkin
+docker build -t "training/departments" departments
+docker build -t "training/users" users
--- users/src/main/java/pl/training/cloud/users/model/User.java
+++ users/src/main/java/pl/training/cloud/users/model/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+@Table(name = "client")
 @Entity
 @Data
 public class User {
